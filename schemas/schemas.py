0599218ad24e2a46d09cf1d8fb2d09c8a4357f0d
tou-py <sebastiantorresg2003@gmail.com>
2025-07-30 16:12:11 -0300

    chore: crea esquemas de datos

--- schemas/schemas.py
+++ schemas/schemas.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import Optional
 
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 
 from models.models import PriorityEnum
 
@@ -15,10 +15,10 @@ class UserBase(BaseModel):
         from_attributes = True
 
 class UserCreate(UserBase):
-    password: str
+    password: str = Field(min_length=8, max_length=64)
 
 
-class UserUpdate(UserBase):
+class UserUpdate(BaseModel):
     first_names: Optional[str] = None
     last_names: Optional[str] = None
     email: Optional[EmailStr] = None
@@ -32,10 +32,10 @@ class UserResponse(UserBase):
 
 
 class TaskBase(BaseModel):
-    title: str
+    title: str = Field(min_length=5, max_length=100)
     description: Optional[str] = None
     completed: bool = False
-    priority: PriorityEnum
+    priority: PriorityEnum = PriorityEnum.PODER
 
     class ConfigDict:
         from_attributes = True
@@ -45,11 +45,13 @@ class TaskCreate(TaskBase):
     user_id: str
 
 
-class TaskUpdate(TaskBase):
+class TaskUpdate(BaseModel):
     title: Optional[str] = None
     description: Optional[str] = None
     completed: Optional[bool] = False
     priority: Optional[PriorityEnum] = None
+    end_at: Optional[datetime] = None
+    finished_at: Optional[datetime] = None
 
 
 class TaskResponse(TaskBase):
