0107d9462860a2efee79708489955844286759ac
tou-py <sebastiantorresg2003@gmail.com>
2025-07-31 00:22:36 -0300

    chore: manejo de excepciones en base_repo y user_repo

--- repositories/base_repo.py
+++ repositories/base_repo.py
@@ -1,5 +1,6 @@
 from typing import TypeVar, Type, Optional, List, Generic
 
+from sqlalchemy.exc import NoResultFound, SQLAlchemyError, IntegrityError
 from sqlmodel import Session, SQLModel, select
 
 Modeltype = TypeVar("Modeltype", bound=SQLModel)
@@ -10,28 +11,65 @@ class BaseRepository(Generic[Modeltype]):
         self.model = model
 
     def get_object_by_id(self, obj_id: str) -> Optional[Modeltype]:
-        return self.session.get(self.model, obj_id)
+        try:
+            return self.session.get(self.model, obj_id)
+        except NoResultFound as ex:
+            raise ex
+        except SQLAlchemyError as ex:
+            raise ex
+        except Exception as ex:
+            raise ex
 
     def get_all(self, offset: int = 0, limit: int = 100) -> List[Modeltype]:
-        statement = select(self.model).offset(offset).limit(limit)
-        return list(self.session.exec(statement).all())
+        try:
+            statement = select(self.model).offset(offset).limit(limit)
+            return list(self.session.exec(statement).all())
+        except SQLAlchemyError as ex:
+            raise ex
 
     def create(self, obj: Modeltype) -> Modeltype:
-        self.session.add(obj)
-        self.session.commit()
-        self.session.refresh(obj)
-        return obj
+        try:
+            self.session.add(obj)
+            self.session.commit()
+            self.session.refresh(obj)
+            return obj
+        except IntegrityError as ex:
+            self.session.rollback()
+            raise ex
+        except SQLAlchemyError as ex:
+            self.session.rollback()
+            raise ex
+        except Exception as ex:
+            self.session.rollback()
+            raise ex
 
     # Aunque resulte repetitivo preferí separar create de update
-    # a pesar de su similaridad y de que SQLModel maneja upsert (actualiza si existe, crea si no)
-    def update(self, obj: Modeltype) -> Optional[Modeltype]:
-        self.session.add(obj)
-        self.session.commit()
-        self.session.refresh(obj)
-        return obj
+    # a pesar de su similitud y de que SQLModel maneja upsert (actualiza si existe, crea si no)
+    def update(self, obj: Modeltype) -> Modeltype:
+        try:
+            self.session.add(obj)
+            self.session.commit()
+            self.session.refresh(obj)
+            return obj
+        except IntegrityError as ex:
+            self.session.rollback()
+            raise ex
+        except SQLAlchemyError as ex:
+            self.session.rollback()
+            raise ex
+        except Exception as ex:
+            self.session.rollback()
+            raise ex
 
 
-    def delete(self, obj_id: str) -> bool:
-        self.session.delete(obj_id)
-        self.session.commit()
-        return True
+    def delete(self, obj: Modeltype) -> bool:
+        try:
+            self.session.delete(obj)
+            self.session.commit()
+            return True
+        except SQLAlchemyError as ex:
+            self.session.rollback()
+            raise ex
+        except Exception as ex:
+            self.session.rollback()
+            raise ex
--- repositories/user_repo.py
+++ repositories/user_repo.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+from pydantic import EmailStr
 from sqlmodel import Session, select
 
 from models.models import User
@@ -10,6 +11,6 @@ class UserRepository(BaseRepository[User]):
     def __init__(self, session: Session):
         super().__init__(session, User)
 
-    def get_user_by_email(self, email: str) -> Optional[User]:
+    def get_user_by_email(self, email: EmailStr) -> Optional[User]:
         statement = select(User).where(User.email == email)
         return self.session.exec(statement).first()
